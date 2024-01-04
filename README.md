44fcc5d7c3b2d6869d87e13b646b5936766cc5e8
Kuba Szczodrzyński <kuba@szczodrzynski.pl>
2024-01-04 23:20:17 +0100

    Generate RBL files with different keys

--- README.md
+++ README.md
@@ -2,8 +2,7 @@
 
 Firmware for dumping encrypted BK72xx bootloaders
 
-<div style="text-align: center">
+<div align="center">
 
 ## [Documentation & usage guide](https://docs.libretiny.eu/docs/platform/beken-72xx/keys/)
-
 </div>
---
+++ post-build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+mkdir -p dist/
+
+envs=("bk7231t" "bk7231n")
+for env in ${envs[@]}; do
+	prefix="dist/${env}-bootloader-dump_v$1"
+	cp ".pio/build/${env}/raw_firmware.bin" "${prefix}.bin"
+	cp ".pio/build/${env}/*.ota.rbl" "${prefix}.default.rbl"
+
+	ltchiptool soc bkpackager ota "${prefix}.bin" "${prefix}.no-aes.rbl" gzip none
+	ltchiptool soc bkpackager ota "${prefix}.bin" "${prefix}.sber.rbl" gzip aes256 --key BDB4CE110F4787C2F539BF50E30A14E0 --iv 46DE464946314329
+done
